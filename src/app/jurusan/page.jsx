b5f6409b9e5c38084681b995c53573e669c87458
Ziyad <kakangtea74@gmail.com>
2024-07-24 16:12:30 +0700

    making layout for jurusan page

---
+++ src/app/jurusan/page.jsx
@@ -0,0 +1,13 @@
+'use client'
+
+export default function JurusanPage() {
+    return (
+        <>
+            <div className="lg:px-20 px-5 py-5 min-h-screen flex justify-center">
+                <div className="w-full max-w-screen-2xl relative ">
+                    
+                </div>
+            </div>
+        </>
+    )
+}
