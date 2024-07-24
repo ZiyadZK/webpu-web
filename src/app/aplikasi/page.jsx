6479642229af0adf05ee6007d2a8521f3c1d937e
Ziyad <kakangtea74@gmail.com>
2024-07-24 16:12:19 +0700

    making layout for aplikasi page

---
+++ src/app/aplikasi/page.jsx
@@ -0,0 +1,88 @@
+'use client'
+
+import MainLayoutPage from "@/components/mainLayout"
+
+export default function AplikasiPage() {
+    return (
+        <>
+            <div className="lg:px-20 px-5 py-5 min-h-screen flex justify-center">
+                <div className="max-w-screen-2xl w-full flex justify-center items-center">
+                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-10">
+                        <a href="https://simak.smkpunegerijabar.sch.id" target="_blank" className="border rounded-2xl p-5 hover:border-zinc-100/0 hover:shadow-2xl ease-out duration-200">
+                            <h1 className="font-bold text-center text-xl lg:text-4xl">
+                                SI<span className="text-amber-500">MAK</span>
+                            </h1>
+                            <hr className="my-2 opacity-0" />
+                            <p className="text-center">
+                                Sistem Informasi Manajemen Akademik
+                            </p>
+                        </a>
+                        <a href="https://simak.smkpunegerijabar.sch.id" target="_blank" className="border rounded-2xl p-5 hover:border-zinc-100/0 hover:shadow-2xl ease-out duration-200">
+                            <h1 className="font-bold text-center text-xl lg:text-4xl">
+                                SI<span className="text-amber-500">MAK</span>
+                            </h1>
+                            <hr className="my-2 opacity-0" />
+                            <p className="text-center">
+                                Sistem Informasi Manajemen Akademik
+                            </p>
+                        </a>
+                        <a href="https://simak.smkpunegerijabar.sch.id" target="_blank" className="border rounded-2xl p-5 hover:border-zinc-100/0 hover:shadow-2xl ease-out duration-200">
+                            <h1 className="font-bold text-center text-xl lg:text-4xl">
+                                SI<span className="text-amber-500">MAK</span>
+                            </h1>
+                            <hr className="my-2 opacity-0" />
+                            <p className="text-center">
+                                Sistem Informasi Manajemen Akademik
+                            </p>
+                        </a>
+                        <a href="https://simak.smkpunegerijabar.sch.id" target="_blank" className="border rounded-2xl p-5 hover:border-zinc-100/0 hover:shadow-2xl ease-out duration-200">
+                            <h1 className="font-bold text-center text-xl lg:text-4xl">
+                                SI<span className="text-amber-500">MAK</span>
+                            </h1>
+                            <hr className="my-2 opacity-0" />
+                            <p className="text-center">
+                                Sistem Informasi Manajemen Akademik
+                            </p>
+                        </a>
+                        <a href="https://simak.smkpunegerijabar.sch.id" target="_blank" className="border rounded-2xl p-5 hover:border-zinc-100/0 hover:shadow-2xl ease-out duration-200">
+                            <h1 className="font-bold text-center text-xl lg:text-4xl">
+                                SI<span className="text-amber-500">MAK</span>
+                            </h1>
+                            <hr className="my-2 opacity-0" />
+                            <p className="text-center">
+                                Sistem Informasi Manajemen Akademik
+                            </p>
+                        </a>
+                        <a href="https://simak.smkpunegerijabar.sch.id" target="_blank" className="border rounded-2xl p-5 hover:border-zinc-100/0 hover:shadow-2xl ease-out duration-200">
+                            <h1 className="font-bold text-center text-xl lg:text-4xl">
+                                SI<span className="text-amber-500">MAK</span>
+                            </h1>
+                            <hr className="my-2 opacity-0" />
+                            <p className="text-center">
+                                Sistem Informasi Manajemen Akademik
+                            </p>
+                        </a>
+                        <a href="https://simak.smkpunegerijabar.sch.id" target="_blank" className="border rounded-2xl p-5 hover:border-zinc-100/0 hover:shadow-2xl ease-out duration-200">
+                            <h1 className="font-bold text-center text-xl lg:text-4xl">
+                                SI<span className="text-amber-500">MAK</span>
+                            </h1>
+                            <hr className="my-2 opacity-0" />
+                            <p className="text-center">
+                                Sistem Informasi Manajemen Akademik
+                            </p>
+                        </a>
+                        <a href="https://simak.smkpunegerijabar.sch.id" target="_blank" className="border rounded-2xl p-5 hover:border-zinc-100/0 hover:shadow-2xl ease-out duration-200">
+                            <h1 className="font-bold text-center text-xl lg:text-4xl">
+                                SI<span className="text-amber-500">MAK</span>
+                            </h1>
+                            <hr className="my-2 opacity-0" />
+                            <p className="text-center">
+                                Sistem Informasi Manajemen Akademik
+                            </p>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
