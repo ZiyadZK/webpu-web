d0af4f1e231c7851cad0ccc8931fbf62ef0007d0
Ziyad <kakangtea74@gmail.com>
2024-08-07 16:31:17 +0700

    reconfiguring navbar layout

--- src/components/mainDashboard.jsx
+++ src/components/mainDashboard.jsx
@@ -2,7 +2,7 @@
 
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { usePathname } from "next/navigation"
+import { usePathname, useRouter } from "next/navigation"
 
 const link = [
     { name: 'Dashboard', link: '/dashboard', role: ['Admin', 'Operator']},
@@ -16,19 +16,19 @@ const link = [
 
 export default function MainDashboardPage({ children }) {
     const pathname = usePathname()
-
+    const router = useRouter()
 
     return (
-        <div className="flex flex-col lg:flex-row gap-3 relative">
-            <div className="w-full lg:w-1/5 flex-shrink-0 flex lg:flex-col flex-row gap-10 lg:gap-5 relative overflow-auto *:flex-shrink-0 *:py-5 *:lg:py-0">
-                {link.map((value, index) => (
-                    <a key={index} href={value.link} className={`md:text-lg hover:underline ease-out duration-200 opacity-60 hover:opacity-100 lg:flex lg:items-center lg:justify-between ${pathname === value.link && 'border-b-4 lg:border-b-0 border-zinc-700'}`}>
-                        {value.name}
-                        <FontAwesomeIcon icon={faArrowRight} className={`w-3 h-3 text-inherit opacity-0 lg:block hidden ${pathname === value.link && 'lg:opacity-100'}`} />
-                    </a>
-                ))}
-            </div>
-            <div className="w-full lg:w-4/5 rounded-lg border p-5">
+        <div className="flex justify-center gap-3 relative">
+            <div className="max-w-screen-xl w-full">
+                <div className="p-3 rounded-xl border flex items-center relative overflow-auto *:flex-shrink-0">
+                    {link.map((value, index) => (
+                        <button key={index} onClick={() => router.push(value['link'])} type="button" className={`px-3 py-2 rounded-md font-medium ${pathname === value.link ? 'bg-zinc-100 hover:bg-zinc-200' : 'hover:bg-zinc-100'} ease-out duration-200`}>
+                            {value['name']}
+                        </button>
+                    ))}
+                </div>
+                <hr className="my-5" />
                 {children}
             </div>
         </div>
