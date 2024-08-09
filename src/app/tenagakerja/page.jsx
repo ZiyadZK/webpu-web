dbd1af42a9e1bbda7b6877184d9d6fe86e4e4e2b
Ziyad <kakangtea74@gmail.com>
2024-08-09 16:11:05 +0700

    syncing tenaga kerja page

--- src/app/tenagakerja/page.jsx
+++ src/app/tenagakerja/page.jsx
@@ -1,60 +1,64 @@
 'use client'
 
 import MainLayoutPage from "@/components/mainLayout"
+import { lexand } from "@/lib/fonts"
+import { M_Tenaga_Kerja_getAll } from "@/models/M_Tenaga_Kerja"
 import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { useEffect, useState } from "react"
 
 export default function TenagaKerjaPage() {
+
+    const [dataTenagaKerja, setDataTenagaKerja] = useState({
+        data: [], filteredData: [], loading: ''
+    })
+
+    const getDataTenagaKerja = async () => {
+        setDataTenagaKerja(state => ({...state, loading: 'loading'}))
+        const response = await M_Tenaga_Kerja_getAll()
+        if(response.success) {
+            setDataTenagaKerja(state => ({...state, data: response.data, filteredData: response.data}))
+        }
+        setDataTenagaKerja(state => ({...state, loading: 'fetched'}))
+    }
+
+    useEffect(() => {
+        getDataTenagaKerja()
+    }, [])
+
     return (
         <MainLayoutPage>
             <div className="lg:px-20 px-5 py-5 min-h-screen flex justify-center">
-                <div className="max-w-screen-2xl w-full ">
-                    <div className="flex justify-center items-center w-full gap-5">
-                        <button type="button">
-                            <FontAwesomeIcon icon={faAngleLeft} className="w-4 h-4 text-inherit" />
-                        </button>
-                        <p>
-                            Page 1
-                        </p>
-                        <button type="button">
-                            <FontAwesomeIcon icon={faAngleRight} className="w-4 h-4 text-inherit" />
-                        </button>
-                    </div>
+                <div className={`max-w-screen-xl w-full ${lexand.className}`}>
+
                     <hr className="my-5 opacity-0" />
-                   <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10">
-                        {Array.from({ length: 30}).slice(0, 9).map((_, index) => (
-                            <div key={index} className="">
+                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
+                        {dataTenagaKerja['filteredData'].map((value, index) => (
+                            <div key={value['id_tenaga_kerja']} className="rounded-xl p-5 col-span-1">
                                 <div className="flex justify-center">
-                                    <div className="flex-shrink-0 w-40 h-40 lg:w-80 lg:h-80 ">
-                                        <img className="w-full h-full object-cover object-center rounded-full" src="https://img.okezone.com/content/2019/01/25/205/2009314/ditelantarkan-agensi-shannon-williams-siap-angkat-kaki-dari-korea-E5N6eFmDFX.jpg" alt="" />
+                                    <div className="aspect-square w-40 sm:w-60 lg:w-80 relative">
+                                        <img className="w-full h-full object-cover rounded-full object-center border" src={`${process.env.NEXT_PUBLIC_API_PUBLIC_URL}/v1/data/foto/${value['foto_profil']['nama_file']}${value['foto_profil']['tipe']}`} alt="" />
+                                        <div className="absolute bottom-0 left-0 w-full">
+                                            <div className="flex justify-center w-full">
+                                                <div className="px-3 py-2 rounded-full bg-white border shadow-xl">
+                                                    <h1 className=" sm:text-lg lg:text-xl tracking-tighter opacity-70">
+                                                        {value['role']}
+                                                    </h1>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                                 <hr className="my-2 opacity-0" />
-                                <h1 className="text-center text-xl lg:text-3xl">
-                                    Ziyad Jahizh Kartiwa
+                                <h1 className="text-lg sm:text-xl lg:text-2xl text-center">
+                                    {value['nama_pegawai']}
                                 </h1>
-                                <hr className="my-1 opacity-0" />
-                                <p className="text-center opacity-60">
-                                    Kepala Sekolah
-                                </p>
                                 <hr className="my-2 opacity-0" />
-                                <p className="text-center font-light border-y py-5">
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti quam porro obcaecati, quidem totam saepe quis suscipit adipisci doloribus praesentium numquam dolor, repudiandae doloremque, placeat officia soluta dicta vero ab!
+                                <p className="font-light text-center">
+                                    {value['quotes']}
                                 </p>
                             </div>
                         ))}
-                   </div>
-                   <hr className="my-5 opacity-0" />
-                   <div className="flex justify-center items-center w-full gap-5">
-                        <button type="button">
-                            <FontAwesomeIcon icon={faAngleLeft} className="w-4 h-4 text-inherit" />
-                        </button>
-                        <p>
-                            Page 1
-                        </p>
-                        <button type="button">
-                            <FontAwesomeIcon icon={faAngleRight} className="w-4 h-4 text-inherit" />
-                        </button>
                     </div>
                 </div>
             </div>
